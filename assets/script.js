c2da68a4c10a3b2e79f69833e67d5333906f3ea1
FrAndroid <70262448+FrAndroidd@users.noreply.github.com>
2024-03-18 16:32:27 -0300

    Translate button + autodetect

--- assets/script.js
+++ assets/script.js
@@ -6,10 +6,10 @@ fetch("https://discord.com/api/guilds/1192002404711927878/widget.json")
 
         if (data.presence_count === 0) {
             statusDot.style.backgroundColor = "#555"
-            statusDot.title = "Desconectado"
+            statusDot.title = "Offline"
         } else {
             statusDot.style.backgroundColor = "#3AB2A4"
-            statusDot.title = "Conectado | Discord"
+            statusDot.title = "Online | Discord"
         }
     }
 )
@@ -62,6 +62,50 @@ randomBackground()
 function show() { // This is run when the background is loaded
     setTimeout(function() {
         document.getElementById("container").style.display = "flex"
-        document.getElementById("backgroundCredits").style.display = "block"
+        document.getElementById("miniContainer").style.display = "flex"
     }, 500)
 }
+
+
+
+// Change language
+languageText = {
+    "es": {
+        "lang.age": "17 años",
+        "lang.bio1": "Hola, soy FrAndroid, un aficionado al software, programador principiante y diseñador de UI/UX como hobby",
+        "lang.bio2": "Actualmente tengo conocimientos relativamente avanzados sobre Windows y Linux",
+        "lang.bio3": "Sé HTML y CSS, programar en Batch y JavaScript, y estoy aprendiendo otros lenguajes",
+        "lang.backgroundBy": "Fondo por",
+        "lang.backgroundAt": "en"
+    },
+    "en": {
+        "lang.age": "17 years old",
+        "lang.bio1": "Hi, I'm FrAndroid, a software enthusiast, beginner programmer and UI/UX designer as a hobby",
+        "lang.bio2": "I currently have a somewhat advanced knowledge about Windows and Linux",
+        "lang.bio3": "I know HTML and CSS, coding in Batch and JavaScript, and I'm learning other programming languages",
+        "lang.backgroundBy": "Background by",
+        "lang.backgroundAt": "@"
+    }
+}
+
+userLanguage = navigator.language.slice(0, 2)
+switchLanguage = 0
+
+function language() {
+    if (switchLanguage === 1) {
+        if (userLanguage === "es") {
+            userLanguage = "en"
+        } else {
+            userLanguage = "es"
+        }
+    }
+    for (let i = 0; i <= Object.keys(languageText.es).length-1; i++) {
+        if (userLanguage === "es") {
+            document.getElementById(Object.keys(languageText.es)[i]).textContent = Object.values(languageText.es)[i]
+        } else {
+            document.getElementById(Object.keys(languageText.en)[i]).textContent = Object.values(languageText.en)[i]
+        }
+    }
+    switchLanguage = 1
+}
+language()
