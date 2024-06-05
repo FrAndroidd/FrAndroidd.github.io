f2e735f2fba765070adbabab791c3a035f1c47d7
FrAndroid <70262448+FrAndroidd@users.noreply.github.com>
2024-06-05 14:53:39 -0300

    Improvements

--- assets/script.js
+++ assets/script.js
@@ -51,8 +51,9 @@ const backgroundList = [
 ]
 
 function randomBackground() {
-    selectedBackground = backgroundList[Math.floor(Math.random() * backgroundList.length)]
+    let selectedBackground = backgroundList[Math.floor(Math.random() * backgroundList.length)]
     document.getElementById("background").src = "assets/backgrounds/" + selectedBackground
+    // Uses regex to turn the background filename into something searchable on Unsplash
     document.getElementById("backgroundLink").href = "https://unsplash.com/s/users/" + selectedBackground.replace(/([A-Z])/g, "-$1").trim().slice(1,-4)
     document.getElementById("backgroundLink").innerText = selectedBackground.slice(0,-4).replace(/([A-Z])/g, " $1").trim() + " @ Unsplash"
 }
@@ -87,13 +88,16 @@ const languageText = {
 const languageTextLength = Object.keys(languageText.es).length - 1
 
 const userLanguage = navigator.language.slice(0, 2)
-if (userLanguage == "es") {
-    setLanguage("es")
-} else {
+// Fallback to english if language not available
+if (languageText[userLanguage] == undefined) {
     setLanguage("en")
+} else {
+    setLanguage(userLanguage)
 }
 
 function setLanguage(targetLanguage) {
+    // Goes through each text in the languageText object, and for each entry in the object, it takes the name of the entry
+    // and replaces the text of the HTML element with the same name as that, to the content of that entry
     for (let i = 0; i <= languageTextLength; i++) {
         document.getElementById(Object.keys(languageText[targetLanguage])[i]).textContent = Object.values(languageText[targetLanguage])[i]
     }
